09174ee2b79a332c1eed2620ad20c91953d36c1c
colorlessenergy <brianmunozdev@gmail.com>
2017-03-19 09:23:48 -0700

    made some inputs work

--- main.js
+++ main.js
@@ -5,3 +5,18 @@
 // around part of an expression is that if removing matching parentheses around a section of text still
 //leaves that section enclosed by parentheses, then those parentheses should be removed as extraneous.
 // ((a((bc)(de)))f) does not have any extra parentheses. Removing any matching set of parentheses does not leave a "single" parenthesesed group that was previously enclosed by the parentheses in question.
+
+
+//input
+
+var userInput = "((2))";
+
+var arrInput = userInput.split("");
+
+if (arrInput[0] === "(" && arrInput[arrInput.length-1] === ")") {
+  console.log(arrInput[0]);
+  console.log(arrInput.length - 1);
+  arrInput.splice(arrInput[0], 1);
+  arrInput.splice(arrInput[arrInput.length - 3], 1);
+  console.log(arrInput);
+}
